2606a7291afeb86bb5dc3cf3a0127ce8affa50b5
Priyansh-J-Shah <spriyansh78@gmail.com>
2022-10-27 13:00:59 +0530

    Project Configuration

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+Medical_CSV_FILE_PATH = os.getcwd() + os.path.join("\\Model","medical_insurance.csv")
+
+Medical_MODEL_FILE_PATH = os.getcwd() + os.path.join("\\Model","Linear_Regression_Medical_Insurance.pkl")
+
+Medical_JSON_FILE_PATH = os.getcwd() + os.path.join("\\Model","Linear_Regression_Medical_Insurance.json")
+
+PORT_NUMBER = 5200
+
